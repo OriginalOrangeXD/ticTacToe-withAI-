f60d4f052f87ed46dd9363e12edbd0706e45ba76
Robert <deangelis.robert13@gmail.com>
2020-01-16 17:00:40 -0500

    More stuff done

--- ticTacToe/main.cpp
+++ ticTacToe/main.cpp
@@ -7,9 +7,21 @@
 //
 
 #include <iostream>
+#include <vector>
+
 using namespace std;
+
+void printBoard(vector<int> &moves);
 
 int main(int argc, const char * argv[]) {
     cout << "Hello, World!\n";
     return 0;
+}
+
+void printBoard(vector<vector<string>> &moves)
+{
+    for(unsigned int i = 0; i < 3; i++){
+        cout << moves[i][0] << '|' << moves[i][1] << '|' << moves[i][2] << endl;
+        cout << "______" << endl;
+    }
 }
