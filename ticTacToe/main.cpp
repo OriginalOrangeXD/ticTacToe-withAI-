268b7c1cb3da536d0c4fdb8c5aee1c27321fac9b
Robert <deangelis.robert13@gmail.com>
2020-01-24 21:14:52 -0500

    Working minimax algorithmic with c++

--- ticTacToe/main.cpp
+++ ticTacToe/main.cpp
@@ -39,10 +39,10 @@ int main(int argc, const char * argv[]) {
         updatePosition(nextMove, count, positions);
         isWin = checkWin(positions);
         if(isWin != 0){
-            if(isWin == -1){
+            if(isWin == 1){
                 cout << "X wins" << endl;
                 game = false;
-            }else if(isWin == 1){
+            }else if(isWin == -1){
                 cout << "O wins" << endl;
                 game = false;
             }
@@ -61,13 +61,13 @@ vector<int> aiMove(vector<string> positions){
     for(int i = 0; i < 3; i++){
         for(int x = 0; x < 3; x++){
             if(positions[i][x] == ' '){
-                positions[i][x] = 'O';
-                score = minimax(positions, 0, true);
+                positions[i][x] = 'X';
+                score = minimax(positions, 0, false);
                 positions[i][x] = ' ';
 //                cout << score << endl;
 //                cout << bestScore << endl;
                 if(score > bestScore){
-//                    cout << "score is bigger than best score " << endl;
+                    cout << "score is bigger than best score " << endl;
                     bestScore = score;
                     bestMove[0] = i;
                     bestMove[1] = x;
@@ -79,7 +79,6 @@ vector<int> aiMove(vector<string> positions){
 }
 int minimax(vector<string>position, int depth, bool isMax){
     int isWin = checkWin(position);
-    cout << isWin << endl;
     if(isWin != 4){
 //        cout << "terminal" << endl;
         return isWin;
@@ -88,7 +87,7 @@ int minimax(vector<string>position, int depth, bool isMax){
         for(unsigned int i = 0; i < 3; i++){
             for(unsigned int x = 0; x < 3; x++){
                 if(position[i][x] == ' '){
-                    position[i][x] = 'O';
+                    position[i][x] = 'X';
                     int score = minimax(position, depth+1, false);
                     position[i][x] = ' ';
                     bestScore = max(score, bestScore);
@@ -102,7 +101,7 @@ int minimax(vector<string>position, int depth, bool isMax){
             for(unsigned int i = 0; i < 3; i++){
                 for(unsigned int x = 0; x < 3; x++){
                     if(position[i][x] == ' '){
-                        position[i][x] = 'X';
+                        position[i][x] = 'O';
                         int score = minimax(position, depth+1, true);
                         position[i][x] = ' ';
                         bestScore = min(score, bestScore);
@@ -121,11 +120,11 @@ int checkWin(vector<string> &position){
     if((position[i][0] == position[i][1]) and (position[i][1] == position[i][2]) and (position[i][0] != ' ')){
         win = position[i][0];
     }else if((position[0][i] == position[1][i]) and (position[1][i] == position[2][i]) and (position[0][i] != ' ')){
-        win = position[i][0];
+        win = position[0][i];
     }else if((position[0][0] == position[1][1]) and (position[1][1] == position[2][2]) and (position[0][0] != ' ')){
-        win = position[i][0];
+        win = position[0][0];
     }else if((position[2][0] == position[1][1]) and (position[1][1] == position[0][2]) and (position[2][0] != ' ')){
-        win = position[i][0];
+        win = position[2][0];
     }
         }
     for (unsigned int x = 0; x < 3;x++){
@@ -136,9 +135,9 @@ int checkWin(vector<string> &position){
         }
     }
     if(win == 'X'){
-        return -1;
-    }else if(win == 'O'){
         return 1;
+    }else if(win == 'O'){
+        return -1;
     }else if(count == 9){
         return 0;
     }else{
@@ -173,16 +172,15 @@ vector<int> getMoves(int count, vector<string> &moves){
 //        cin >> oMoveY;
 //        move.push_back(oMoveX);
 //        move.push_back(oMoveY);
-        return aiMove(moves);
-
-    }else{
         int xMoveX;
         int xMoveY;
-        cout << "Enter your move player X in the format '3\n2'" << endl;
+        cout << "Enter your move player O in the format '3\n2'" << endl;
         cin >> xMoveX;
         cin >> xMoveY;
         move.push_back(xMoveX);
         move.push_back(xMoveY);
+    }else{
+        return aiMove(moves);
     }
     
     return move;
